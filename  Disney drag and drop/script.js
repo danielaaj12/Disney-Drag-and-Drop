6cd9d852a29c1e8a3c084425d876783fa94fd0d6
Hilary Cordero Flores <h.cordero@WEB-07.local>
2018-12-05 16:19:46 -0600

    cambios html y js

---  Disney drag and drop/script.js
+++  Disney drag and drop/script.js
@@ -1,55 +1,46 @@
-const fill = document.querySelector('.foto');
+// princesas
+const fills = document.querySelectorAll('.foto');
 const empties = document.querySelectorAll('.empty');
-console.log(fill);
-// Fill listeners
-fill.addEventListener('dragstart', dragStart);
-fill.addEventListener('dragend', dragEnd);
-
+let counter = 0;
 // arreglo
-let imagenes = [];
-imagenes [0] = 'img/ariel.jpg';
-imagenes[1] = 'img/blancanieves.png';
-imagenes[2] = 'img/cenicienta.png';
-imagenes[3] = 'img/merida.png';
-imagenes[4] = 'img/tiana.png';
-imagenes[5] = 'img/bella.png';
+let imagenes = [
+  'ariel.jpg',
+  'blancanieves.png',
+  'cenicienta.jpg',
+  'merida.jpg',
+  'tiana.png',
+  'bella.png'
+];
 
-document.fill = imagenes;
-document.fill2 = imagenes[1];
-
+// Fill listeners
+for (const fill of fills) {
+  fill.children[0].src = 'img/'+imagenes[counter];
+  fill.setAttribute('data', counter);
+  fill.addEventListener('dragstart', () => {
+    fill.className += ' hold';
+    num = fill.getAttribute('data')
+    setTimeout(() => (fill.className = 'invisible'), 0);
+  });
+  fill.addEventListener('dragend', () => {
+    fill.className = 'fill';
+  });
+  counter++;
+}
 // Loop through empty boxes and add listeners
 for (const empty of empties) {
-  empty.addEventListener('dragover', dragOver);
-  empty.addEventListener('dragenter', dragEnter);
-  empty.addEventListener('dragleave', dragLeave);
-  empty.addEventListener('drop', dragDrop);
-}
-
-// Drag Functions
-
-function dragStart() {
-  this.className += ' hold';
-  setTimeout(() => (this.className = 'invisible'), 0);
-}
-
-function dragEnd() {
-  this.className = 'fill';
-}
-
-function dragOver(e) {
-  e.preventDefault();
-}
-
-function dragEnter(e) {
-  e.preventDefault();
-  this.className += ' hovered';
-}
-
-function dragLeave() {
-  this.className = 'empty';
-}
-
-function dragDrop() {
-  this.className = 'empty';
-  this.append(fill);
+  empty.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+  empty.addEventListener('dragenter', (e) => {
+    e.preventDefault();
+    empty.className += ' hovered';
+  });
+  empty.addEventListener('dragleave', () => {
+    empty.className = 'empty';
+  });
+  empty.addEventListener('drop', () => {
+    empty.className = 'empty';
+    empty.append(fills[num]);
+    // console.log(fills.indexOf(fills[0]));
+  });
 }
